0910ef65c247f1a800d9b2fae225712375944cca
Matt Schrader <mschrader@gmrmarketing.com>
2017-12-01 15:21:21 -0600

    Fix control keys

--- keyboards/levinson/keymaps/haxix/keymap.c
+++ keyboards/levinson/keymaps/haxix/keymap.c
@@ -45,10 +45,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `----------------------------------------------------------------------------------------------'
  */
 [QWERTY_LAYER] = KEYMAP( \
-  KC_GRV,   KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,           KC_BSPC, \
-  KC_TAB,   KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,        KC_ENT,  \
-  KC_LSPO,  KC_Z,           KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,        KC_RSPC, \
-  KC_LCBR,  CTL_T(KC_LCBR), KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_SPC,  KC_RGUI, KC_RALT, CTL_T(KC_RBRC), KC_RCBR  \
+  KC_GRV,   KC_Q,                 KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,                 KC_BSPC, \
+  KC_TAB,   KC_A,                 KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,              KC_ENT,  \
+  KC_LSPO,  KC_Z,                 KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,              KC_RSPC, \
+  KC_LCBR,  MT(MOD_LCTL,KC_LBRC), KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_SPC,  KC_RGUI, KC_RALT, MT(MOD_LCTL,KC_RBRC), KC_RCBR  \
 ),
 
 /* Lower
