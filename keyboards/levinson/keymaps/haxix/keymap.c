5634aed4ca9fdcc1d7a3a959008fbc1f6c101f4f
Matt Schrader <SchraderMJ11@gmail.com>
2017-11-30 22:12:24 -0600

    Add haxix

---
+++ keyboards/levinson/keymaps/haxix/keymap.c
@@ -0,0 +1,177 @@
+#include "levinson.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+
+enum haxix_layers {
+	QWERTY_LAYER,
+	LOWER_LAYER,
+	RAISE_LAYER,
+	NAV_LAYER,
+	ADJUST_LAYER
+};
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+	NAV,
+	SEND_VERSION
+};
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,----------------------------------------------------------------------------------------------.
+ * | `      |   Q    |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P      | Bksp    |
+ * |--------+--------+------+------+------+-------------+------+------+------+----------+---------|
+ * | Tab    |   A    |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;      | Enter   |
+ * |--------+--------+------+------+------+------|------+------+------+------+----------+---------|
+ * | Shift (|   Z    |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /      | Shift ) |
+ * |--------+--------+------+------+------+------+------+------+------+------+----------+---------|
+ * | {      | Ctrl [ | Alt  | GUI  |Lower |Space |Space |Raise | GUI  | Alt  |  Ctrl ]  | }       |
+ * `----------------------------------------------------------------------------------------------'
+ */
+[QWERTY_LAYER] = KEYMAP( \
+  KC_GRV,   KC_Q,           KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,           KC_BSPC, \
+  KC_TAB,   KC_A,           KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,        KC_ENT,  \
+  KC_LSPO,  KC_Z,           KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,        KC_RSPC, \
+  KC_LCBR,  CTL_T(KC_LCBR), KC_LALT, KC_LGUI, KC_SPC,  LOWER,   RAISE,   KC_SPC,  KC_RGUI, KC_RALT, CTL_T(KC_RBRC), KC_RCBR  \
+),
+
+/* Lower
+ * ,-----------------------------------------------------------------------------------.
+ * |  Esc |  !  |   @  |   #  |   $  |   %   |   ^  |   &  |   *  |   \  |   |  | Del  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   "  |      |      |      |      |
+ * |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[LOWER_LAYER] = KEYMAP( \
+	KC_ESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_BSLS, KC_PIPE , KC_DEL, \
+	KC_F1,  KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_UNDS, KC_QUOT, _______, _______, _______, _______, \
+	KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   _______, _______, _______, _______, _______, _______, \
+	_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+),
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |  UP  |      |      |      |  /   |  7   |  8   |  9   |  *   |      |
+ * |      | LEFT | DOWN | RGHT |      |      |  -   |  4   |  5   |  6   |  +   |      |
+ * |      |      |      |      |      |      |      |  1   |  2   |  3   |  =   |      |
+ * |      |      |      |      |      |      |      |      |  0   |  .   |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[RAISE_LAYER] = KEYMAP( \
+	_______, _______, KC_UP  , _______, _______, _______, KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PAST, _______,  \
+	_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_PPLS, _______,  \
+	_______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   KC_PEQL, _______,  \
+	_______, _______, _______, _______, _______, _______, _______, _______, KC_P0,   KC_PDOT, _______, _______   \
+),
+
+/* Nav
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[NAV_LAYER] =  KEYMAP( \
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+),
+
+/* Adjust (Lower + Raise)
+ * ,-----------------------------------------------------------------------------------.
+ * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty|      |      |      |      |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |             |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[ADJUST_LAYER] =  KEYMAP( \
+  _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+  _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+)
+};
+
+#ifdef AUDIO_ENABLE
+float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
+#endif
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        persistent_default_layer_set(1UL<<QWERTY_LAYER);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(LOWER_LAYER);
+        update_tri_layer(LOWER_LAYER, RAISE_LAYER, ADJUST_LAYER);
+      } else {
+        layer_off(LOWER_LAYER);
+        update_tri_layer(LOWER_LAYER, RAISE_LAYER, ADJUST_LAYER);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(RAISE_LAYER);
+        update_tri_layer(LOWER_LAYER, RAISE_LAYER, ADJUST_LAYER);
+      } else {
+        layer_off(RAISE_LAYER);
+        update_tri_layer(LOWER_LAYER, RAISE_LAYER, ADJUST_LAYER);
+      }
+      return false;
+      break;
+		case NAV:
+			if (record->event.pressed) {
+				layer_on(NAV_LAYER);
+			} else {
+				layer_off(NAV_LAYER);
+			}
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(ADJUST_LAYER);
+      } else {
+        layer_off(ADJUST_LAYER);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
